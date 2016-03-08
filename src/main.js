d06f41a3e18d58a4d33b505cadecd58b9edfc284
Michael Orchard <morchard@scottlogic.co.uk>
2016-03-08 13:25:18 +0000

    Slightly better naming.

--- src/main.js
+++ src/main.js
@@ -5,29 +5,29 @@ import {Observable, BehaviorSubject} from 'rx';
 import {run} from '@cycle/core';
 import {makeDOMDriver, div} from '@cycle/dom';
 
-const initialTabs = [
+const initialSVGs = [
     { name: 'svg 1', data: '<rect x="5" y="5" height="10" width="10" fill="green"/>' },
     { name: 'svg 2', data: '<rect x="20" y="25" height="15" width="10" fill="blue"/>' }
 ];
 
 function main(sources) {
-    const tabs$ = new BehaviorSubject(storage.get('svg-data') || initialTabs);
-    const names$ = tabs$.map(tabs => tabs.map(tab => tab.name));
+    const svgs$ = new BehaviorSubject(storage.get('svg-data') || initialSVGs);
+    const names$ = svgs$.map(svgs => svgs.map(svg => svg.name));
     const tabs = Tabs({ DOM: sources.DOM, names$: names$ });
 
     const selection$ = tabs.selection$;
-    const data$ = Observable.combineLatest(tabs$, selection$, (tabs, selection) => tabs[selection].data);
-    const editor = SVGEditor({ DOM: sources.DOM, data$: data$ });
+    const currentData$ = Observable.combineLatest(svgs$, selection$, (svgs, selection) => svgs[selection].data);
+    const editor = SVGEditor({ DOM: sources.DOM, data$: currentData$ });
     const input$ = editor.input$;
     input$.subscribe(function(value) {
-        const tabData = tabs$.getValue();
+        const svgs = svgs$.getValue();
         const selection = selection$.getValue();
-        tabData[selection].data = value;
-        tabs$.onNext(tabData);
+        svgs[selection].data = value;
+        svgs$.onNext(svgs);
     });
 
-    tabs$.subscribe(function(data) {
-        storage.set('svg-data', data);
+    svgs$.subscribe(function(tabs) {
+        storage.set('svg-data', tabs);
     });
 
     const vtabs$ = tabs.DOM;
